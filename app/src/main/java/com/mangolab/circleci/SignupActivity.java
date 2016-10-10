b6cb34564e4adbbbd47f4aba03c27db457814c7c
rajforhad <RAjnsu@8>
2016-10-10 16:46:29 +0100

    3rd testing check circleci

---
+++ app/src/main/java/com/mangolab/circleci/SignupActivity.java
@@ -0,0 +1,17 @@
+package com.mangolab.circleci;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rajforhad on 10/10/2016.
+ */
+
+public class SignupActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
