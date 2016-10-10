ca139b853259a7aa79f56a07cacd6b019d1cde94
rajforhad <RAjnsu@8>
2016-10-10 16:22:34 +0100

    Initial commit with my android circleci project

---
+++ app/src/main/java/com/mangolab/circleci/MainActivity.java
@@ -0,0 +1,22 @@
+package com.mangolab.circleci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
